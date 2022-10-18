900ee7a0a4c3facea7afc64d8ee28d2c19893101
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-19 00:27:23 +0300

    move: `{Random}` root namespace -> `Numerics`

--- Numerics/Random.cs
+++ Numerics/Random.cs
@@ -1,13 +1,13 @@
 ﻿/// —————————————————————————————————————————————
-//? 
+//?
 //!? 📜 Random.cs
 //!? 🖋️ Galacticai 📅 2022
 //!  ⚖️ GPL-3.0-or-later
 //?  🔗 Dependencies: No special dependencies
-//? 
+//?
 /// —————————————————————————————————————————————
 
-namespace GalacticLib.Math {
+namespace GalacticLib.Math.Numerics {
     public static class Random {
         /// <returns> Random <see cref="int"/> such as:
         /// <br/> <paramref name="max"/> &lt;= <see cref="int"/> &lt;= <paramref name="max"/></returns>
@@ -18,7 +18,7 @@ namespace GalacticLib.Math {
         public static int GetRandom(int min, int max)
             => new System.Random().Next(min, max + 1); //? +1 to match the max
 
-        /// <summary> Chooses a random object from input array </summary> 
+        /// <summary> Chooses a random object from input array </summary>
         /// <returns>Random object from input array</returns>
         public static T FromArray<T>(T[] array)
             => array[GetRandom(0, array.Length - 1)]; //? -1 because it starts from 0
